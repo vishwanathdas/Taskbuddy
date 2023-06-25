d3fb9c2235dbf299527479cacc4419df6eae3828
Vishwanath Das <111034515+vishwanathdas@users.noreply.github.com>
2023-06-25 16:23:28 +0530

    Delete TaskBuddy directory

--- TaskBuddy/account/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from account.models import UserProfile
-
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
--- TaskBuddy/task/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from task.models import Task
-
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    pass
